c8bcde8fa944d60f96d34e11566d6b15499200ca
DawiMessias <wtih123@gmail.com>
2022-02-03 22:04:03 -0300

    Component Summary;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from "../src/styles/global";
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -12,18 +12,18 @@ export const Content = styled.div `
     align-items: center;
     justify-content: space-between;
 
-    button {
-     font-size: 1rem;
-     color: #fff;
-     background: var(--blue-ligth);
-     border: 0;
-     padding: 0 2rem;
-     border-radius: 0.25rem;
-     height: 3rem;
-     transition: filter(0.2s);
+        button {
+        font-size: 1rem;
+        color: #fff;
+        background: var(--blue-ligth);
+        border: 0;
+        padding: 0 2rem;
+        border-radius: 0.25rem;
+        height: 3rem;
+        transition: filter(0.2s);
 
-     &:hover {
-         filter: brightness(0.9)
-     }
+        &:hover {
+            filter: brightness(0.9)
+        }
     }
 `
